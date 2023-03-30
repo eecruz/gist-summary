73cfbda9950225aaf21698ea0724a66b720baae2
Emilio Cruz <eecruz@quinnpiac.edu>
2023-03-30 16:03:56 -0400

    adjusted api interface

--- app/src/main/java/edu/quinnipiac/ser210/gistsummary/ApiInterface.kt
+++ app/src/main/java/edu/quinnipiac/ser210/gistsummary/ApiInterface.kt
@@ -1,18 +1,41 @@
 package edu.quinnipiac.ser210.gistsummary
 
+import org.w3c.dom.Text
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
+import retrofit2.http.Body
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Headers
+import retrofit2.http.POST
 
 interface ApiInterface
 {
-    //@GET("marvel/")
-    //fun getHeroes() : Call<ArrayList<Hero?>?>?
+    @POST("extractive/summarize-url/")
+    @Headers("Accept:application/json", "Content-Type:application/json",
+             "Authorization: Bearer d859bd9d8emsh7665e099afce6e3p13227djsnf07373f301cb")
+    fun generateUrlSummary(@Body params: SummarySpecs) : Call<TextSummary>
+
+    @POST("abstractive/summarize-url/")
+    @Headers("Accept:application/json", "Content-Type:application/json",
+        "Authorization: Bearer d859bd9d8emsh7665e099afce6e3p13227djsnf07373f301cb")
+    fun generateHumanUrlSummary(@Body params: SummarySpecs) : Call<TextSummary>
+
+    @POST("extractive/summarize-text/")
+    @Headers("Accept:application/json", "Content-Type:application/json",
+        "Authorization: Bearer d859bd9d8emsh7665e099afce6e3p13227djsnf07373f301cb")
+    fun generateTextSummary(@Body params: SummarySpecs) : Call<TextSummary>
+
+    @POST("abstractive/summarize-text/")
+    @Headers("Accept:application/json", "Content-Type:application/json",
+        "Authorization: Bearer d859bd9d8emsh7665e099afce6e3p13227djsnf07373f301cb")
+    fun generateHumanTextSummary(@Body params: SummarySpecs) : Call<TextSummary>
+
+
 
     companion object
     {
-        var BASE_URL = "https://simplifiedcoding.net/demos/"
+        var BASE_URL = "https://tldrthis.p.rapidapi.com/v1/model/"
 
         fun create(): ApiInterface
         {
--- app/src/main/java/edu/quinnipiac/ser210/gistsummary/TextSummary.kt
+++ app/src/main/java/edu/quinnipiac/ser210/gistsummary/TextSummary.kt
@@ -1,3 +1,4 @@
 package edu.quinnipiac.ser210.gistsummary
 
-data class TextSummary(var summary: String)
+data class TextSummary(var summary: String?)
+data class SummarySpecs(var text: String?, var num_sentences: Int?, var is_detailed: Boolean?)
